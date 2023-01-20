5de68bf16749a0dd1a35fd0c72b74cf2fedf7f89
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-01-21 00:18:35 +0300

    BRAND-ETicaret
    PHP

---
+++ nedmin/netting/baglan.php
@@ -0,0 +1,15 @@
+<?php 
+
+try {
+
+	$db=new PDO("mysql:host=localhost;dbname=eticaret;charset=utf8",'root','');
+	//echo "veritabanı bağlantısı başarılı";
+}
+
+catch (PDOExpception $e) {
+
+	echo $e->getMessage();
+}
+
+
+ ?>
---
+++ nedmin/netting/islem.php
@@ -0,0 +1,176 @@
+<?php
+ob_start();
+session_start();
+
+include 'baglan.php';
+
+if (isset($_POST['admingiris'])) {
+
+	$kullanici_mail=$_POST['kullanici_mail'];
+	$kullanici_password=md5($_POST['kullanici_password']);
+
+	$kullanicisor=$db->prepare("SELECT * FROM kullanici where kullanici_mail=:mail and kullanici_password=:password and kullanici_yetki=:yetki");
+	$kullanicisor->execute(array(
+		'mail' => $kullanici_mail,
+		'password' => $kullanici_password,
+		'yetki' => 5
+		));
+
+	echo $say=$kullanicisor->rowCount();
+
+	if ($say==1) {
+				
+		$_SESSION['kullanici_mail']=$kullanici_mail;
+		header("Location:../production/index.php");
+		exit;
+
+
+
+	} else {
+
+		header("Location:../production/login.php?durum=no");
+		exit;
+	}
+	
+
+}
+
+
+
+if (isset($_POST['genelayarkaydet'])) {
+	
+	//Tablo güncelleme işlemi kodları...
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		ayar_title=:ayar_title,
+		ayar_description=:ayar_description,
+		ayar_keywords=:ayar_keywords,
+		ayar_author=:ayar_author
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+		'ayar_title' => $_POST['ayar_title'],
+		'ayar_description' => $_POST['ayar_description'],
+		'ayar_keywords' => $_POST['ayar_keywords'],
+		'ayar_author' => $_POST['ayar_author']
+		));
+
+
+	if ($update) {
+
+		header("Location:../production/genel-ayar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/genel-ayar.php?durum=no");
+	}
+	
+}
+
+
+
+if (isset($_POST['iletisimayarkaydet'])) {
+	
+	//Tablo güncelleme işlemi kodları...
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		ayar_tel=:ayar_tel,
+		ayar_gsm=:ayar_gsm,
+		ayar_faks=:ayar_faks,
+		ayar_mail=:ayar_mail,
+		ayar_ilce=:ayar_ilce,
+		ayar_il=:ayar_il,
+		ayar_adres=:ayar_adres,
+		ayar_mesai=:ayar_mesai
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+		'ayar_tel' => $_POST['ayar_tel'],
+		'ayar_gsm' => $_POST['ayar_gsm'],
+		'ayar_faks' => $_POST['ayar_faks'],
+		'ayar_mail' => $_POST['ayar_mail'],
+		'ayar_ilce' => $_POST['ayar_ilce'],
+		'ayar_il' => $_POST['ayar_il'],
+		'ayar_adres' => $_POST['ayar_adres'],
+		'ayar_mesai' => $_POST['ayar_mesai']
+		));
+
+
+	if ($update) {
+
+		header("Location:../production/iletisim-ayarlar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/iletisim-ayarlar.php?durum=no");
+	}
+	
+}
+
+
+if (isset($_POST['apiayarkaydet'])) {
+	
+	//Tablo güncelleme işlemi kodları...
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		
+		ayar_analystic=:ayar_analystic,
+		ayar_maps=:ayar_maps,
+		ayar_zopim=:ayar_zopim
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+
+		'ayar_analystic' => $_POST['ayar_analystic'],
+		'ayar_maps' => $_POST['ayar_maps'],
+		'ayar_zopim' => $_POST['ayar_zopim']
+		));
+
+
+	if ($update) {
+
+		header("Location:../production/api-ayarlar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/api-ayarlar.php?durum=no");
+	}
+	
+}
+
+
+if (isset($_POST['hakkimizdakaydet'])) {
+	
+	//Tablo güncelleme işlemi kodları...
+
+	/*
+
+	copy paste işlemlerinde tablo ve işaretli satır isminin değiştirildiğinden emin olun!!!
+
+	*/
+	$ayarkaydet=$db->prepare("UPDATE hakkimizda SET
+		hakkimizda_baslik=:hakkimizda_baslik,
+		hakkimizda_icerik=:hakkimizda_icerik,
+		hakkimizda_video=:hakkimizda_video,
+		hakkimizda_vizyon=:hakkimizda_vizyon,
+		hakkimizda_misyon=:hakkimizda_misyon
+		WHERE hakkimizda_id=0");
+
+	$update=$ayarkaydet->execute(array(
+		'hakkimizda_baslik' => $_POST['hakkimizda_baslik'],
+		'hakkimizda_icerik' => $_POST['hakkimizda_icerik'],
+		'hakkimizda_video' => $_POST['hakkimizda_video'],
+		'hakkimizda_vizyon' => $_POST['hakkimizda_vizyon'],
+		'hakkimizda_misyon' => $_POST['hakkimizda_misyon']
+		));
+
+
+	if ($update) {
+
+		header("Location:../production/hakkimizda.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/hakkimizda.php?durum=no");
+	}
+	
+}
+
+?>
