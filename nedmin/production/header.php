051a47a99c706dd6aae3ca118f81c79180518e37
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-01-21 19:25:14 +0300

    Template ekleme ve parçalama

--- nedmin/production/header.php
+++ nedmin/production/header.php
@@ -47,7 +47,7 @@ $ayarcek=$ayarsor->fetch(PDO::FETCH_ASSOC);
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>BRAND</span></a>
+              <a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>BRAND</span></a>
             </div>
 
             <div class="clearfix"></div>
