68c2ce957c0611d0e6b42c39159f279d9f02eff6
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-01-22 23:49:23 +0300

    menuleri listeleme

--- nedmin/production/header.php
+++ nedmin/production/header.php
@@ -95,6 +95,7 @@ if ($say==0) {
                   <li><a href="index.php"><i class="fa fa-home"></i> Anasayfa</a></li>
                   <li><a href="hakkimizda.php"><i class="fa fa-info"></i> Hakkimizda </span></a>
                   <li><a href="kullanici.php"><i class="fa fa-user"></i> Kullanıcılar </span></a>
+                  <li><a href="menu.php"><i class="fa fa-list"></i> Menüler </span></a>
                   <li><a><i class="fa fa-cogs"></i> Ayarlar <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="genel-ayar.php">Genel Ayar</a></li>
---
+++ nedmin/production/menu.php
@@ -0,0 +1,107 @@
+<?php 
+
+include 'header.php'; 
+
+//Belirli veriyi seçme işlemi
+$menusor=$db->prepare("SELECT * FROM menu");
+$menusor->execute();
+
+?>
+
+
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Menü Listeleme <small>
+
+              <?php 
+
+              if (isset($_GET['durum'])&&$_GET['durum']=="ok") {?>
+
+              <b style="color:green;">İşlem Başarılı...</b>
+
+              <?php } elseif (isset ($_GET['durum'])&&$_GET['durum']=="no") {?>
+
+              <b style="color:red;">İşlem Başarısız...</b>
+
+              <?php }
+
+              ?>
+
+
+            </small></h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li>
+                
+                <a href="menu-ekle.php"><button class="btn btn-success btn-xs">Menu Ekle</button></a>
+              </li>
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+              <li class="dropdown">
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+            
+          </div>
+          <div class="x_content">
+
+
+            <!-- Div İçerik Başlangıç -->
+
+            <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+              <thead>
+                <tr>
+                  <th>Menü Sıra</th>
+                  <th>Menu Ad</th>
+                  <th>Üst Menü</th>
+                  <th>Menü URL</th>
+                  <th>Menü Durumu</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+
+              <tbody>
+
+                <?php 
+
+                while($menucek=$menusor->fetch(PDO::FETCH_ASSOC)) {?>
+                <tr>
+                  <td width="5%"><?php echo $menucek['menu_sira'] ?></td>
+                  <td><?php echo $menucek['menu_ad'] ?></td>
+                  <td><?php echo $menucek['menu_ust'] ?></td>
+                  <td><?php echo $menucek['menu_url'] ?></td>
+                  <td><?php echo ($menucek['menu_durum'])=='1'?"Aktif":"Pasif" ?></td>
+                  <td width="5%"><center><a href="menu-duzenle.php?menu_id=<?php echo $menucek['menu_id']; ?>"><button class="btn btn-primary btn-xs">Düzenle</button></a></center></td>
+                  <td width="5%"><center><a href="../netting/islem.php?menu_id=<?php echo $menucek['menu_id']; ?>&menusil=ok"><button class="btn btn-danger btn-xs">Sil</button></a></center></td>
+                </tr>
+                <?php  }
+
+                ?>
+
+
+              </tbody>
+            </table>
+
+            <!-- Div İçerik Bitişi -->
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+  </div>
+</div>
+<!-- /page content -->
+
+<?php include 'footer.php'; ?>
