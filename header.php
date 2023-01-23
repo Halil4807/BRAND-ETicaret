8e8a9759ce855f3e42384ad8174b271efbe1ff03
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-01-23 23:51:56 +0300

    slider index ekranında listelenmesi

--- header.php
+++ header.php
@@ -140,68 +140,73 @@ $menusor->execute(array('durum'=>1));
 
 
 
-    											    											?>
+    											?>
 
 
 
 
     											"><?php echo $menucek["menu_ad"]; ?></a></li>
-    									<?php } ?>
+    										<?php } ?>
 
 
-    								</ul>
-    							</div>
-    						</div>
-    						<div class="col-md-2 machart">
-    							<button id="popcart" class="btn btn-default btn-chart btn-sm "><span class="mychart">Cart</span>|<span class="allprice">$0.00</span></button>
-    							<div class="popcart">
-    								<table class="table table-condensed popcart-inner">
-    									<tbody>
-    										<tr>
-    											<td>
-    												<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
-    											</td>
-    											<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
-    											<td>1X</td>
-    											<td>$138.80</td>
-    											<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
-    										</tr>
-    										<tr>
-    											<td>
-    												<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
-    											</td>
-    											<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
-    											<td>1X</td>
-    											<td>$138.80</td>
-    											<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
-    										</tr>
-    										<tr>
-    											<td>
-    												<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
-    											</td>
-    											<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
-    											<td>1X</td>
-    											<td>$138.80</td>
-    											<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
-    										</tr>
-    									</tbody>
-    								</table>
-    								<span class="sub-tot">Sub-Total : <span>$277.60</span> | <span>Vat (17.5%)</span> : $36.00 </span>
-    								<br>
-    								<div class="btn-popcart">
-    									<a href="checkout.htm" class="btn btn-default btn-red btn-sm">Checkout</a>
-    									<a href="cart.htm" class="btn btn-default btn-red btn-sm">More</a>
+    									</ul>
     								</div>
-    								<div class="popcart-tot">
-    									<p>
-    										Total<br>
-    										<span>$313.60</span>
-    									</p>
-    								</div>
-    								<div class="clearfix"></div>
     							</div>
+    							<div class="col-md-2 machart">
+    								<button id="popcart" class="btn btn-default btn-chart btn-sm "><span class="mychart">Cart</span>|<span class="allprice">$0.00</span></button>
+    								<div class="popcart">
+    									<table class="table table-condensed popcart-inner">
+    										<tbody>
+    											<tr>
+    												<td>
+    													<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
+    												</td>
+    												<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
+    												<td>1X</td>
+    												<td>$138.80</td>
+    												<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
+    											</tr>
+    											<tr>
+    												<td>
+    													<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
+    												</td>
+    												<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
+    												<td>1X</td>
+    												<td>$138.80</td>
+    												<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
+    											</tr>
+    											<tr>
+    												<td>
+    													<a href="product.htm"><img src="images\dummy-1.png" alt="" class="img-responsive"></a>
+    												</td>
+    												<td><a href="product.htm">Casio Exilim Zoom</a><br><span>Color: green</span></td>
+    												<td>1X</td>
+    												<td>$138.80</td>
+    												<td><a href=""><i class="fa fa-times-circle fa-2x"></i></a></td>
+    											</tr>
+    										</tbody>
+    									</table>
+    									<span class="sub-tot">Sub-Total : <span>$277.60</span> | <span>Vat (17.5%)</span> : $36.00 </span>
+    									<br>
+    									<div class="btn-popcart">
+    										<a href="checkout.htm" class="btn btn-default btn-red btn-sm">Checkout</a>
+    										<a href="cart.htm" class="btn btn-default btn-red btn-sm">More</a>
+    									</div>
+    									<div class="popcart-tot">
+    										<p>
+    											Total<br>
+    											<span>$313.60</span>
+    										</p>
+    									</div>
+    									<div class="clearfix"></div>
+    								</div>
+    							</div>
+    							<ul class="small-menu"><!--small-nav -->
+    								<li><a href="" class="myacc">My Account</a></li>
+    								<li><a href="" class="myshop">Shopping Chart</a></li>
+    								<li><a href="" class="mycheck">Checkout</a></li>
+    							</ul><!--small-nav -->
     						</div>
     					</div>
     				</div>
-    			</div>
 	</div><!--end main-nav -->
---
+++ slider.php
@@ -0,0 +1,51 @@
+<?php 
+$slidersor=$db->prepare("SELECT * FROM slider");
+$slidersor->execute();
+$slidersor2=$db->prepare("SELECT * FROM slider");
+$slidersor2->execute();
+?>		
+<div class="main-slide">
+	<div id="sync1" class="owl-carousel">
+
+<?php  while($slidercek=$slidersor->fetch(PDO::FETCH_ASSOC)) {?>
+
+		<div class="item">
+			<div class="slide-desc">
+				<div class="inner">
+					<h1>Stylish Jacket, be your best deal</h1>
+					<p>
+						Nunc non fermentum nunc. Sed ut ante eget leo tempor consequat sit amet eu orci. Donec dignissim dolor eget..
+					</p>
+					<button class="btn btn-default btn-red btn-lg">Add to cart</button>
+				</div>
+				<div class="inner">
+					<div class="pro-pricetag big-deal">
+						<div class="inner">
+							<span class="oldprice">$314</span>
+							<span>$199</span>
+							<span class="ondeal">Best Deal</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="slide-type-1">
+				<a href="<?php echo $slidercek["slider_link"] ?>"> <img src="<?php echo $slidercek['slider_resimyol'] ?>" alt="" class="img-responsive"></a>
+			</div>
+		</div>
+
+<?php } ?>
+
+	</div>
+</div>	
+<div class="bar"></div>
+<div id="sync2" class="owl-carousel">
+
+<?php  while($slidercek2=$slidersor2->fetch(PDO::FETCH_ASSOC)) {?>
+	<div class="item">
+		<div class="slide-type-1-sync">
+			<h3><?php echo $slidercek2['slider_ad'] ?></h3>
+			<p>Description here here here</p>
+		</div>
+	</div>
+<?php } ?>
+</div>
